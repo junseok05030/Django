eabf75a9d84233309df8e5695176f2690cba2b4a
junseok05030 <164356571+junseok05030@users.noreply.github.com>
2024-12-17 19:39:06 +0900

    커미리림리미릴미림리림

--- Django/locallibrary/catalog/admin.py
+++ Django/locallibrary/catalog/admin.py
@@ -6,5 +6,5 @@ from catalog.models import Author, Genre, Book, BookInstance
 
 admin.site.register(Book)
 admin.site.register(Author)
-admin.site.registeR(Genre)
+admin.site.register(Genre)
 admin.site.register(BookInstance)
